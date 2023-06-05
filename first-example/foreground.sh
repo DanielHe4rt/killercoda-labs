5c72873b9b7ed73ccad1c2e4bf7371b19c1b81a4
danielhe4rt <hey@danielheart.dev>
2023-06-05 13:02:33 -0300

    wip

--- first-example/foreground.sh
+++ first-example/foreground.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-set -x
 echo "Waiting for the environment get ready."
-while [ ! -f /tmp/background0 ]; do sleep 1; done
+while [ ! -f /tmp/background0 ]; do sleep 1; done &>/dev/null
 
-source ~/.bashrc
+source ~/.bashrc &>/dev/null
 echo "Done! Welcome to the ScyllaDB Training."
