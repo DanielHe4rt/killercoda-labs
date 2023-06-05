e0f760b78e0a3c03158c6727fa205f55a00e6214
danielhe4rt <hey@danielheart.dev>
2023-06-05 13:29:54 -0300

    wip

--- first-example/background.sh
+++ first-example/background.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 set -x
-curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh && sh nodesource_setup.sh && sudo apt install nodejs -y && rm -rf nodesource_setup.sh
 cat ~/.bashrc .dh-bashrc >> ~/.bashrc
 source ~/.bashrc
 
+curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh && sh nodesource_setup.sh && sudo apt install nodejs -y
 rm -rf nodesource_setup.sh .dh-bashrc
 sleep 1
 echo done > /tmp/background0
