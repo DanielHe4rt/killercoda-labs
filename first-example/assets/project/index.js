0187d52296e1d1abb1d65021d4e47e3d83ebab84
danielhe4rt <hey@danielheart.dev>
2023-06-14 16:21:29 -0300

    wip

--- first-example/assets/project/index.js
+++ first-example/assets/project/index.js
@@ -1,21 +1,25 @@
+const streamers = require('./streamers.json');
 const tmi = require('tmi.js');
 const cassandra = require('cassandra-driver');
+cassandra_drv.types.consistencies.localQuorum
 
 const client = new tmi.Client({
-    channels: ['danielhe4rt'],
+    channels: ['your-twitch-channel', 'danielhe4rt'],
+    // channels: streamers.map((streamer) => streamer.streamer_username),
     joinInterval: 300
 });
 
 const cluster = new cassandra.Client({
     contactPoints: ["172.17.0.2", "172.17.0.3", "172.17.0.4"],
-    credentials: {username: 'scylla', password: 'secret-password'},
+    credentials: { username: 'scylla', password: 'secret-password' },
     localDataCenter: 'datacenter1',
     keyspace: 'twitch_sentinel'
 })
 
 async function insertOnDatabase(user, message) {
+    message = escape(message);
     let query = `INSERT INTO messages (streamer_id, chatter_id, chatter_username, chatter_message, message_sent_at) VALUES ('${user['room-id']}', '${user['user-id']}', '${user['display-name']}', '${message}', ${user['tmi-sent-ts']})`
-     await cluster.execute(query);
+    await cluster.execute(query);
 }
 
 
